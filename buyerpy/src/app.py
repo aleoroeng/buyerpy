07b50529ecc3538d19724eb6948a22d038223ee7
aleoroeng <oro99eng@gmail.com>
2021-09-18 21:05:48 -0400

    added skeleton endpoints

--- buyerpy/src/app.py
+++ buyerpy/src/app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template
 
-DEBUG = True
+
 app = Flask(__name__)
+stores = [
+    {
+        "name": "My store",
+        "items": [
+            {
+                "name": "my item",
+                "price": 15.99
+            }
+        ]
+    }
+]
 
 @app.route("/")
 def home():
     return "Hello, world!"
-@app.route("/index")
-def index():
-    return render_template("main.html", name="main")
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
 
-app.run(port=5000)
+@app.route("/store/<string:name>")
+def get_store_by_name():
+    pass
+@app.route("/store")
+def get_stores():
+    pass
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store():
+    pass
+@app.route("/store/<string:name>/item")
+def get_items_in_store():
+    pass
+app.run(port=5000,debug=True)
 
